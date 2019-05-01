c320dc99512a1af001aeacb052f35e3ebb06314b
Fatai Balogun <fattylee.remod@gmail.com>
2019-05-01 14:41:05 +0100

    nesting component

--- src/app.js
+++ src/app.js
@@ -44,7 +44,71 @@ const render = () => {
   </div>
 );
 
-ReactDOM.render(template, appRoot);
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+};
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div>
+        {(app && app.title) && <h1>{app.title}</h1>}
+        {(app.subTitle) && <p>{app.subTitle}</p>}
+      </div>
+    );
+  }
+};
+
+class Action extends React.Component {
+  render () {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+};
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        Options Component Here
+        <Option />
+      </div>
+    );
+  }
+};
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option Component Here
+      </div>
+    );
+  }
+};
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        AddOption Component Here
+      </div>
+    );
+  }
+};
+
+
+ReactDOM.render(<IndecisionApp />, appRoot);
 
 };
 
