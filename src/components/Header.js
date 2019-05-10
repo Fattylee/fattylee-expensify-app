5b14c02526d6e34bbd42324c1861b74a8f20df77
Fatai Balogun <fattylee.remod@gmail.com>
2019-05-10 17:53:18 +0100

    rebuild bundle.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,7 @@ const Header = ({ title, subTitle }) => {
     return (
       <div className='header'>
         <div className='container'>
-          <h1 className='header__title'>{title}
-            {/* <span class="material-icons">face</span> */}
-            <span class="mi mi-face">hy</span>
-          </h1>
+          <h1 className='header__title'>{title}</h1>
           {subTitle && <p className='header__subtitle'>{subTitle}</p>}
         </div>
       </div>
